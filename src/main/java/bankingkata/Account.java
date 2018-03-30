a60d82fb8d8174da7f1e785ef90579dafbc042ac
Andre Kappes <lewwerworscht@vollbio.de>
2018-03-30 23:00:00 +0200

    Add gitignore, pom and interface class

---
+++ src/main/java/bankingkata/Account.java
@@ -0,0 +1,14 @@
+package bankingkata;
+
+public class Account {
+
+    public void deposit(int amount) {
+    }
+
+    public void withdraw(int amount) {
+    }
+
+    public void printStatement() {
+    }
+
+}
---
+++ src/test/java/bankingkata/AccountTest.java
@@ -0,0 +1,5 @@
+package bankingkata;
+
+public class AccountTest {
+
+}
